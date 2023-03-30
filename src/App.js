317beb082cec077fd51db4c3025822062fa9eef1
Dilip <dilip.884400@gmail.com>
2023-03-30 22:59:32 +0530

    assignment done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import GenerateNo from './guess/guess.js'
 
 function App() {
   return (
     <div>
      <Navbar/>
+     {/* <GenerateNo /> */}
     </div>
   );
 }
