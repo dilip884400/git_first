87771a325f9d5162945a4557d453fe81ab232245
Dilip <dilip.884400@gmail.com>
2023-04-25 23:23:25 +0530

    Assignment Done redux-day2

---
+++ src/components/assignment.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import Button from "./button";
+import { Provider } from "react-redux";
+import { store } from "./store";
+
+export default function Assignment() {
+  return (
+    <div>
+      <Provider store={store}>
+        <Button />
+      </Provider>
+    </div>
+  );
+}
